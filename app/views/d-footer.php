087a742b57a5c3cc6d46fa9db18446377200e270
grum1n <grumin.el.pastas@gmail.com>
2021-08-08 17:00:51 +0300

    Added button to top

--- app/views/d-footer.php
+++ app/views/d-footer.php
@@ -1,4 +1,5 @@
 <footer>
        <a href="mailto:grumin1979@gmail.com">grumin</a> &copy; <?php echo date('Y'); ?>
 </footer> 
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 <script src="app/js/custom.js"></script>
