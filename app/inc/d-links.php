d8e4239c8759786b321cf715293fb315da84ff9b
grum1n <grumin.el.pastas@gmail.com>
2021-08-08 16:21:45 +0300

    Changed text description in h2 h3

--- app/inc/d-links.php
+++ app/inc/d-links.php
@@ -1,6 +1,6 @@
 <div class="right-container" >
     <div class="section-heading">
-        <h2>LINKS</h2>
+        <h2>WEB LINKS</h2>
         <h3><span>Home</span> / Links</h3>
     </div>
     Links 
