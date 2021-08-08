5579450658d01f9e8aa0fc55f78a8ecdcb607215
grum1n <grumin.el.pastas@gmail.com>
2021-08-08 16:47:22 +0300

    Added new content

--- app/inc/d-links.php
+++ app/inc/d-links.php
@@ -3,5 +3,85 @@
         <h2>WEB LINKS</h2>
         <h3><span>Home</span> / Links</h3>
     </div>
-    Links 
+    <div class="dashboard-grid-2col">
+        <div class="col-1 box-2">
+            <button class="box-accordion">DEVELOPERS LINKS</button>
+                <div class="box-accordion-content">
+                    <p><a href="https://www.figma.com/" target="_blank">Figma</a></p>
+                    <p><a href="https://compressor.io/" target="_blank">Compressor</a></p>
+                    <p><a href="https://validator.w3.org/#validate_by_input" target="_blank">Validate by direct input HTML</a></p>
+                    <p><a href="https://jigsaw.w3.org/css-validator/#validate_by_input" target="_blank">Validate by direct input CSS</a></p>
+                    <p><a href="https://fontawesome.com/v5.15/icons?d=gallery&p=2&m=free" target="_blank">Fontawesome</a></p>
+                    <p><a href="https://fonts.google.com/" target="_blank">Google fonts</a></p>
+                    <p><a href="https://github.com/" target="_blank">Github</a></p>
+                    <p><a href="https://gitlab.com/" target="_blank">Gitlab</a></p>
+                    <p><a href="https://visualstudio.microsoft.com/" target="_blank">Visual Studio</a></p>
+                    <p><a href="https://scout-app.io/" target="_blank">SASS scout-app</a></p>
+                    <p><a href="https://entitycode.com/" target="_blank">Entity code</a></p>
+                    <p><a href="https://www.w3schools.com/colors/colors_picker.asp" target="_blank">W3S color picker</a></p>
+                </div>
+            <button class="box-accordion">LEARN LINKS</button>
+                <div class="box-accordion-content">
+                    <p><a href="https://www.w3schools.com/" target="_blank">W3schools</a></p>
+                    <p><a href="https://htmlreference.io/" target="_blank">HTML reference</a></p>
+                    <p><a href="https://cssreference.io/" target="_blank">CSS reference</a></p>
+                    <p><a href="https://www.codecademy.com/" target="_blank">Codecademy</a></p>
+                    <p><a href="https://developer.mozilla.org/en-US/" target="_blank">developer.mozilla.org</a></p>
+                    <p><a href="https://tympanus.net/codrops/css_reference/" target="_blank">Tympanus</a></p>
+                    <p><a href="https://getmimo.com/invite/tlccvo" target="_blank">Mimo</a></p>
+                    <p><a href="https://codepen.io/enxaneta/full/adLPwv/" target="_blank">Codepen - Flex</a></p>
+                    <p><a href="https://www.vilniuscoding.lt/" target="_blank">Vilnius coding school</a></p>
+                </div>
+            <button class="box-accordion">WINDOWS KEYBOARD SHORTKUTS</button>
+                <div class="box-accordion-content">
+                    <p>Win+A: Open the Action Center.</p>
+                    <p>Win+I: Open Windows Settings.</p>
+                    <p>Win+S: Open Cortana.</p>
+                    <p>Win+C: Open Cortana in listening mode.</p>
+                    <p>Win+Left: Snap current window to the left side of the screen.</p>
+                    <p>Win+Right: Snap current window the the right side of the screen.</p>
+                    <p>Win+Up: Snap current window to the top of the screen.</p>
+                    <p>Win+Down: Snap current window to the bottom of the screen.</p>
+                    <p>Win+Tab: This opens the new Task View.</p>
+                    <p>Alt+Tab lets you switch between your open Windows.</p>
+                    <p>Win+Ctrl+D: Create a new virtual desktop and switch to it.</p>
+                    <p>Win+Ctrl+F4: Close the current virtual desktop.</p>
+                    <p>Win+Ctrl+Left/Right: Switch to the virtual desktop on the left or right.</p>
+                    <p>Ctrl+V or Shift+Insert: Pastes text at the cursor.</p>
+                    <p>Ctrl+C or Ctrl+Insert: Copies the selected text to the clipboard.</p>
+                    <p>Ctrl+A: Select all text in the current line if the line contains text.</p>
+                    <p>Shift+Left/Right/Up/Down: Moves the cursor left a character, right a character, up a line, or down a line, selecting the text along the way. Continue pressing arrow keys to select more text.</p>
+                    <p>Ctrl+Shift+Left/Right: Moves the cursor one word to the left or right, selecting that word along the way.</p>
+                    <p>Shift+Home/End: Moves the cursor to the beginning or end of the current line, selecting text along the way.</p>
+                    <p>Shift+Page Up/Page Down: Moves the cursor up or down a screen, selecting text.</p>
+                    <p>Ctrl+Shift+Home/End: Moves the cursor to the beginning or end of the “screen buffer,” selecting all text between the cursor and the beginning or end of the Command Prompt’s output.</p>
+                    <p>Ctrl+Up/Down: Moves one line up or down in the Command Prompt’s history — it’s like using the scroll bar.</p>
+                    <p>Ctrl+Page Up/Page Down: Moves one page up or down in the Command Prompt’s history — it’s like scrolling even farther.</p>
+                    <p>Ctrl+M: Enter “mark mode,” which helps for selecting text. Previously, the only way to do this was by right-clicking in the Command Prompt and selecting Mark.</p>
+                    <p>Ctrl+F: Opens a Find dialog for searching the Command Prompt’s output.</p>
+                    <p>Alt+F4: Closes the Command Prompt window.</p>
+                </div>
+        </div>
+        <div class="col-2 box-2">
+            <button class="box-accordion">INVEST LINKS</button>
+                <div class="box-accordion-content">
+                    <p><a href="www.profitus.lt/ref/d6m62n4a2c" target="_blank">PROFITUS</a></p>
+                    <p><a href="https://www.paskoluklubas.lt/registracija/investuotojo?ref=MIGR42" target="_blank">Paskolų Klubas</a></p>
+                    <p><a href="https://nordstreet.com/lt/registruotis?referral=NS356990" target="_blank">NORDSTREET</a></p>
+                    <p><a href="https://heavyfinance.eu/lt/registruotis?referral=HF826376" target="_blank">heavyfinance</a></p>
+                </div>
+            <button class="box-accordion">HOSTING LINKS</button>
+                <div class="box-accordion-content">
+                    <p><a href="https://rekomendacija.iv.lt/grum1n1979"  target="_blank">Interneto Vizija</a></p>
+                    <p><a href="https://www.hostinger.lt/" target="_blank">www.hostinger.lt</a></p>
+                    
+                </div>
+            <button class="box-accordion">WORDPRESS LINKS</button>
+                <div class="box-accordion-content">
+                    <p><a href="https://wordpress.org/"  target="_blank">Wordpress.org</a></p>
+                    <p><a href="https://themeforest.net/"  target="_blank">Themeforest.net</a></p>
+                    <p><a href="https://www.elegantthemes.com/"  target="_blank">Elegantthemes.com - DIVI</a></p>
+                </div>
+        </div>
+    </div>
 </div> 
