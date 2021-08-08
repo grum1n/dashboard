adf19344517525d41a79bbe446e1b4698b9d7f75
grum1n <grumin.el.pastas@gmail.com>
2021-08-08 17:06:48 +0300

    Changed button style

--- app/views/d-footer.php
+++ app/views/d-footer.php
@@ -1,5 +1,5 @@
 <footer>
        <a href="mailto:grumin1979@gmail.com">grumin</a> &copy; <?php echo date('Y'); ?>
 </footer> 
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+<button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fas fa-angle-up"></i></button>
 <script src="app/js/custom.js"></script>
