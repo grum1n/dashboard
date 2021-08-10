0812d256b2fa5f4ed76c6433304cb373c9c32175
grum1n <grumin.el.pastas@gmail.com>
2021-08-10 09:59:04 +0300

    Added new info to github-skills file

--- app/inc/github-skills.php
+++ app/inc/github-skills.php
@@ -3,5 +3,105 @@
         <h2>GITHUB</h2>
         <h3><span>Home</span> / Github</h3>
     </div>
-    git hub skills
+    <div class="dashboard-grid-2col">
+        <div class="col-1 panel-headings-left">
+            <h3>GIT WORKFLOW</h3>
+            <p>Write code</p>
+            <p>Stage changes - git add</p>
+            <p>Commit changes - git commit</p>
+            <p>Push changes - git push</p>
+            <p>Make a Pull request</p>
+        </div>
+        <div class="col-2 panel-headings-left">
+            <h3>GIT BRANCHING</h3>
+            <p>* branch -> shows currently branch</p>
+            <p>* checkout -> create new branch</p>
+            <p>* checkout -b test-01 -> created new test-01 branch and switched to it</p>
+            <p>* checkout test-01 -> switches to test-01 branch</p>
+        </div>
+    </div>
+    <div class="dashboard-grid-2col">
+        <div class="col-1 panel-headings-left">
+            <h3>ABOUT GIT</h3>
+            <p>What is Git?</p>
+            <p class="text-italic">A: Free and open source version control system.</p>
+            <p>What is Version Control?</p>
+            <p class="text-italic">A: The management of changes to documents, computer programs, large web sites, and other collections of information.</p>
+            <p class="text-italic">Version Control is basically a way that we as programmers track our
+                                   code changes, we basically save an initial version of our code into Git,
+                                   and then when we update code, we can save it into Git again, and again and again and again.
+                                   And throughout time as our code continues to change, we can look back at all of the changes we have 
+                                   made over time. This helps us to see and understand what we did when, as well as track down bugs, or go
+                                   back to a previous version of the code if we need to.
+                                   
+            </p>
+            <p><a href="https://www.youtube.com/watch?v=RGOj5yH7evk&t=315s">Github for beginners - youtube</a></p>
+        </div>
+        <div class="col-2 panel-headings-left">
+            <h3>Terms</h3>
+            <p>* Directory -> Folder</p>
+            <p>* Terminal or Command Line -> Interface for Text Commands</p>
+            <p>* CLI -> Command Line Interface</p>
+            <p>* cd -> Change Directory</p>
+            <p>* Code Editor -> Word Processor for Writing Code</p>
+            <p>* Repository -> Project, or the folder/place where your projects is kept</p>
+            <p>* Git -> is the tool that tracks changes in your code over time.</p>
+            <p>* Github -> is a website where you host all of your Git repositories. Being online, it makes it 
+                easy to work in groups with other people, and organize your projects into a portfolio for you to
+                show potential employers.
+            </p>
+        </div>
+    </div>
+    <div class="dashboard-grid-2col">
+        <div class="col-1 panel-headings-left">
+            <h3>GIT COMMANDS</h3>
+            <p>* clone -> Bring a repository that is hosted somwhere like Github into a 
+                folder on your local machine.
+            </p>
+            <p>* cd ../folder -> go up to folder</p>
+            <p>* add -> Track your files and changes in Git</p>
+            <p>* commit -> Save your files in Git</p>
+            <p>* push -> Upload Git commits to a remove repo, like Github </p>
+            <p>* push origin main -> "origin"- is an option set for us here,and is basically a word that stands for
+                the location of our Git repository. "main"- is the branch that we want to push to.
+            </p>
+            <p>push -u origin master -> for set upstream and then enter In the future,ican just use git push 
+                without typing out origin main.
+            </p>
+            <p>* pull -> Download changes from remote repo to your local machine, the opposite of push</p>
+            <p>* init -> it says initialize Git repository in ex. test-folder</p>
+            <p>* -m -> for message to commit files, that message should have something to do with the what and why behind the commit you're making.
+                first message is for title box, a second message is for description box.
+            </p>
+            <p>* status -> this command shows all of the files that were updated or created or deleted,byt haven't
+                been saved in a commit yet.
+            </p>
+            <p>* clear -> </p>
+            <p>* ls or ls -la -> least everything , it has all of the changes recorded in the history 
+                of this repository,which includes the ones we made on github
+            </p>
+            <p>* pbcopy < ... -> copy</p>
+            <p>* remote -></p>
+            <p>* remote add origin "ssh-emailtest@gmail.com" -> add origin and paste that link that i copied</p>
+            <p>* remote -v -> shows any remote repositories that I've connected to this repo</p>
+            <p>* branch -> shows currently branch</p>
+            <p>* checkout -> create new branch</p>
+            <p>* checkout -b test-01 -> created new test-01 branch and switched to it</p>
+            <p>* checkout test-01 -> switches to test-01 branch</p>
+        </div>
+        <div class="col-2 panel-headings-left">
+            <h3>GIT SSH KEY</h3>
+            <p>* ssh-keygen -t rsa -b 4096 -C "email@gmail.com" -> make sure that the email address you enter here is the 
+                same one you use to log into your GitHub account
+            </p>
+            <p>* ls | grep testkey -> search generated key</p>
+            <p>* cat testkey.pub -> shows key </p> 
+            <p>* pbcopy < ... -> copy</p>
+            <p>* vim -> ...</p>
+            <p>should be more info..</p>
+        </div>
+    </div>
+    
+    
+
 </div> 
