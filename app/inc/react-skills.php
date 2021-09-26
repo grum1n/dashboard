8f4955af1576e58199d2ec80edaccfc8252c9bd8
grum1n <grumin.el.pastas@gmail.com>
2021-09-26 14:29:58 +0300

    added new page react-skills.php

---
+++ app/inc/react-skills.php
@@ -0,0 +1,28 @@
+<div class="right-container" >
+    <div class="section-heading">
+        <h2>REACT</h2>
+        <h3><span>Home</span> / React</h3>
+    </div>
+    <div class="dashboard-grid-2col">
+        <div class="col-1 panel-headings-left">
+            <h3>React Getting Started</h3>
+            <p>React is Javascript library for building user interfaces.</p>
+        </div>
+        <div class="col-2 panel-headings-left">
+            <h3>React Tutorials</h3>
+            <p>* text</p>
+        </div>
+    </div>
+    <div class="dashboard-grid-2col">
+        <div class="col-1 panel-headings-left">
+            <h3>About React</h3>
+            <p>text</p>
+            <p class="text-italic">next text.</p>
+            <p><a href="https://www.react.org">React  org</a></p>
+        </div>
+        <div class="col-2 panel-headings-left">
+            <h3>React terms</h3>
+            <p>* Directory -> Folder</p>
+        </div>
+    </div>
+</div> 
