d6a664c54723e381e17ed5887b450c312578bb07
grum1n <grumin.el.pastas@gmail.com>
2021-09-26 15:36:38 +0300

    added react terms content

--- app/inc/react-skills.php
+++ app/inc/react-skills.php
@@ -7,22 +7,31 @@
         <div class="col-1 panel-headings-left">
             <h3>React Getting Started</h3>
             <p>React is Javascript library for building user interfaces.</p>
+            
         </div>
         <div class="col-2 panel-headings-left">
             <h3>React Tutorials</h3>
-            <p>* text</p>
+            <p>* <a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=742s" target="_blank">React JS Crash Course 2021</a></p>
         </div>
     </div>
     <div class="dashboard-grid-2col">
         <div class="col-1 panel-headings-left">
-            <h3>About React</h3>
-            <p>text</p>
-            <p class="text-italic">next text.</p>
-            <p><a href="https://www.react.org">React  org</a></p>
+            <h3>React TERMS</h3>
+            <p>.help - Print help message</p>
+            <p>.break - Sometimes you get stuck, this gets you out</p>
+            <p>.clear - Alias for .break</p>
+            <p>.editor - Enter the REPL</p>
+            <p>.exit - Exit the REPL</p>
+            <p>.load - Load JS from a file into yhe REPL session</p>
+            <p>.save - Save all evaluated commands in this REPL session to a file</p>
+            <p>Ctrl+C - abort current expression</p>
+            <p>Ctrl+D - exit the REPL</p>
         </div>
         <div class="col-2 panel-headings-left">
-            <h3>React terms</h3>
-            <p>* Directory -> Folder</p>
+            <h3>React Online Playgrounds</h3>
+            <p>* CodePen</p>
+            <p>* CodeSandbox</p>
+            <p>* Stackblitz</p>
         </div>
     </div>
 </div> 
