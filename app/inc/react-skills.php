7ffe5ed33b91946d3f0dca5a666705e8b0b1efaf
grum1n <grumin.el.pastas@gmail.com>
2021-09-30 16:53:09 +0300

    changed react-skills coontent

--- app/inc/react-skills.php
+++ app/inc/react-skills.php
@@ -6,29 +6,51 @@
     <div class="dashboard-grid-2col">
         <div class="col-1 panel-headings-left">
             <h3>React Getting Started</h3>
-            <p>React is Javascript library for building user interfaces.</p>
             <p>Install Node.js</p>
             <p>1. Command Prompt or  git bash </p>
-            <p>2.cd folderName (find folder where be installed react)</p>
-            <p>3. npm --version (to check node verion)</p>
-            <p>4. npx create-react-app react-task-tracker (install app files)</p>
-            <p>5. cd react-task-tracker ( go to react-task-tracker folder)</p>
-            <p>6. code . (means open vs code editor)</p>
-            <p>7. npm start (started dev server  , lockalhost:3000)</p>
-            <p>8. public/index.html ( is the single page that is loaded,If you open it directly in the browser, you will see an empty page)</p>
-            <p>9. src/index.js (</p>
-            <p>10. in index.html change title ("Task Tracker", changes browser tab name)</p>
-            <p>11. go  in to src/app.js (this is the root app component)</p>
-            <p>12. delete content from section div=app,and change it to hello word. (look at locakhost:3000)</p>
-            <p>13. delete top (import logo from './logo.svg';import './App.css';)</p>
-            <p>14. delete files : App.css, App.test.js, logo.svg, setupTests.js</p>
-            <p>15. all we can put in div classname='App'</p>
+            <p>2.1. Git Bash: cd folderName (find folder where be installed react)</p>
+            <p>2.2. create folder / open / in current folder open Git Bash</p>
+            <p>3.1. Git Bash: npm --version (to check node verion)</p>
+            <p>3.2. Git Bash: npm --v (to check node verion)</p>
+            <p>4.1. Git Bash: npx create-react-app react-task-tracker (create folder and install app)</p>
+            <p>4.2. Git Bash: npx create-react-app ./ (install app files in current folder )</p><p>5. cd react-task-tracker ( go to react-task-tracker folder)</p>
+            <p>6. Git Bash: code . (means open vs code editor)</p>
+            <p>7. VS terminal: npm start (started dev server  , lockalhost:3000)</p>
+            <p>8. VS app.js : change content in header section</p>
+            <p>9. open package.json </p>
+            <p>10. Vs package.json: after "version" add "homepage": "http://grumin.lt/react-test/"</p>
+            <p>11. VS terminal: npm run build</p>
+            <p>12. in /build/ folder: zip all files name ex.. build</p>
+            <p>13. on host server create folder /ex. react-test/</p>
+            <p>14. upload build zip to /ex. react-test/</p>
+            <p>15. open host server direct admin ../ex. react-test/</p>
+            <p>16. extract build.zip in current folder /ex. react-test/</p>
+            <p>17. in /ex. react-test/ delete build.zip file</p>
+            <p>18. open in browser web page :) "http://grumin.lt/react-test/"</p>
+            <p>19. app.js app.css : change content </p>
+            <p>20. VS terminal: nmp run build </p>
+            <p>21. in /build/ folder: zip all files name ex.. build</p>
+            <p>22. upload build zip to /ex. react-test/</p>
+            <p>23. extract build.zip in current folder /ex. react-test/</p>
+            <p>24. in /ex. react-test/ delete build.zip file</p>
+            <p>25. update web page in browser</p>
             <p> ** npm i react-icons</p>
+            <p> ** npm install gh-pages</p>
+            <p> ** npm run deploy</p>
+            <p> ** npm run build && gh-pages -d build</p>
+
 
         </div>
         <div class="col-2 panel-headings-left">
             <h3>React Tutorials</h3>
+            <p>React is Javascript library for building user interfaces. Created by Facebook.</p>
+            <p>React is a tool for building UI components.</p>
+            <p>How does React Work? React creates a VIRTUAL DOM in memory.</p>
+            <p>React only changes what needs to be changed!</p>
+            <p>React finds out what changes have been made, and changes only what needs to be changed.</p>
+            <p>* <a href="https://www.youtube.com/watch?v=aQhAmW_I4II" targer="_blank">How to upload react Projectto Cpanel</a></p>
             <p>* <a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=742s" target="_blank">React JS Crash Course 2021</a></p>
+            <p>* <a href="https://www.w3schools.com/react/react_intro.asp"  target="_blank">React w3school</a></p>
         </div>
     </div>
     <div class="dashboard-grid-2col">
