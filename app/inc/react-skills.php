1ef6f3e54c27a8398075b640d8d6a4cc57d1d39a
grum1n <grumin.el.pastas@gmail.com>
2021-10-02 13:02:20 +0300

    added more info about React

--- app/inc/react-skills.php
+++ app/inc/react-skills.php
@@ -78,9 +78,12 @@
             <h3>LEARN REACT</h3>
             <p class="codetext">import React from 'react';</p>
             <p>This creates an object named React which contains methods necessary to use the React library.</p>
+            <p>import React from 'react' </p>
+            <p>creates a JavaScript object. This object contains properties that are needed to make React work, such as React.createElement() and React.Component</p>
             <p class="codetext">import ReactDOM from 'react-dom';</p>
             <p>This line of code is very similar to line 1.
                Both import JavaScript objects. In both lines, the imported object contains React-related methods.</p>
+            <p>import ReactDOM from 'react-dom' creates another JavaScript object. This object contains methods that help React interact with the DOM, such as ReactDOM.render()</p>
             <p>The methods imported from 'react-dom' are meant for interacting with the DOM. You are already familiar with one of them: ReactDOM.render()</p>
             <p>The methods imported from 'react' don’t deal with the DOM at all. They don’t engage directly with anything that isn’t part of React.</p>
             <p>To clarify: the DOM is used in React applications, but it isn’t part of React. After all, the DOM is also used in countless non-React applications. Methods imported from 'react' are only for pure React purposes, such as creating components or writing JSX elements.</p>
@@ -91,6 +94,65 @@
             <p>React.Component is a JavaScript class. To create your own component class, you must subclass React.Component. You can do this by using the syntax:</p>
             <p>class YourComponentNameGoesHere extends React.Component {}</p>
             <p>You know that you are declaring a new component class, which is like a factory for building React components. You know that React.Component is a class, which you must subclass in order to create a component class of your own. You also know that React.Component is a property on the object which was returned by import React from 'react' on line 1.</p>
+            <p>Component class variable names must begin with capital letters!</p>
+            <p>That name should be written in UpperCamelCase. In this case, your chosen name is MyComponentClass</p>
+            <p>Like all JavaScript classes, this one needs a body. The body will act as a set of instructions, explaining to your component class how it should build a React component.</p>
+            <p>Here’s what your class body would look like on its own, without the rest of the class declaration syntax</p>
+            <pre>
+                <p class="codetext">{
+  render() {
+    return Hello world;
+  }
+}
+                </p>
+            </pre>
+            <pre>
+                <p class="codetext">
+class ComponentFactory extends React.Component {
+// instructions go here, between the curly braces
+}
+                </p>
+            </pre>
+            <p>these instructions should take the form of a class declaration body. That means that they will be delimited by curly braces</p>
+            <p>There is only one property that you have to include in your instructions: a render method.</p>
+            <p>A render method is a property whose name is render, and whose value is a function. The term “render method” can refer to the entire property, or to just the function part.</p>
+            <pre>
+                <p class="codetext">
+class ComponentFactory extends React.Component {
+  render() {}
+}
+                </p>
+            </pre>
+            <p>A render method must contain a return statement. Usually, this return statement returns a JSX expression:</p>
+            <pre>
+                <p class="codetext">
+class ComponentFactory extends React.Component {
+  render() {
+    return &#60;h1&#62;Hello world&#60;/h1&#62;;
+  }
+}
+                </p>
+            </pre>
+            <p>All you know so far is that its name is render, it needs a return statement for some reason, and you have to include it in the body of your component class declaration.</p>
+            <p>So, let’s make a React component! It only takes one more line:</p>
+            <pre>
+                <p class="codetext">
+&#60;MyComponentClass /&#62;
+                </p>
+            </pre>
+            <p>Whenever you make a component, that component inherits all of the methods of its component class. MyComponentClass has one method: MyComponentClass.render(). Therefore, <MyComponentClass /> also has a method named render</p>
+            <p>You could make a million different <MyComponentClass /> instances, and each one would inherit this same exact render method.</p>
+            <p>To call a component’s render method, you pass that component to ReactDOM.render(). Notice your component, being passed as ReactDOM.render()‘s first argument:</p>
+            <pre>
+                <p class="codetext">
+ReactDOM.render(
+    &#60;MyComponentClass /&#62;,
+    document.getElementById('app')
+);
+                </p>
+            </pre> 
+            <p>ReactDOM.render() will tell <MyComponentClass /> to call its render method.</p> 
+            <p><MyComponentClass /> will call its render method, which will return the JSX element <h1>Hello world</h1>. ReactDOM.render() will then take that resulting JSX element, and add it to the virtual DOM. This will make “Hello world” appear on the screen.</p>    
         </div>
         <div class="col-2 panel-headings-left">
             <h3>REACT EXAMPLE</h3>
@@ -100,10 +162,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class MyComponentClass extends React.Component {
-  render() {
+  render() { // render is method
     return Hello world;
   }
 }
+
+ReactDOM.render(
+  &#60;MyComponentClass /&#62;,
+  document.getElementById('app')
+);
             </p>
             </pre>
         </div>
